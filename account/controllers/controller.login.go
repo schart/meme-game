a9769cbcd56bad30eac00b6f4a07f98f1e9a1495
Heja <arslanheja202@gmail.com>
2023-09-28 15:59:31 +0300

    Modified login

--- account/controllers/controller.login.go
+++ account/controllers/controller.login.go
@@ -1,26 +1,30 @@
 package controllers
 
 import (
-	"fmt"
 	"net/http"
 	accountQueries "shared-library/database/queries/queries-account"
+	"shared-library/interceptors"
+	myJwt "shared-library/jwt"
 	accountTypes "shared-library/types/account"
 	"shared-library/utils"
+	"time"
 )
 
-func AccountLogin(w http.ResponseWriter, r *http.Request) {
+func AccountLoginController(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain")
-
 	// Check method
 	checkMethod := utils.HttpMethodSet(http.MethodPost, r)
 	if checkMethod != true {
 		utils.HandleError(w, http.StatusMethodNotAllowed, "Method error expected method "+http.MethodPost)
 		return
 	}
-	fmt.Println("Test cookie: ", r.Cookies())
 
 	// Check token
-	// here
+	statusToken := interceptors.TokenCheck(w, r)
+	if statusToken == true {
+		utils.HandleError(w, http.StatusUnauthorized, "You already have a session")
+		return
+	}
 
 	// Get parsed body
 	formData := utils.ParsedBodyGet(w, r)
@@ -44,5 +48,21 @@ func AccountLogin(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Create token
+	token, err := myJwt.TokenCreate(formData.Get("Username"))
+	if err != nil {
+		utils.HandleError(w, http.StatusExpectationFailed, err.Error())
+		return
+	}
+
+	// Create cookie
+	cookie := &http.Cookie{
+		Name:    "token",
+		Value:   token,
+		Expires: time.Now().Add(24 * time.Hour), // change the time
+	}
+
+	// Publish the token
+	http.SetCookie(w, cookie)
 	utils.HandleSuccess(w, []string{})
 }
