075348063a00233ae43a6bdad1bda598ae38567c
Heja <arslanheja202@gmail.com>
2023-10-11 15:36:38 +0300

    Set-ed endpoints for get rooms

---
+++ game/controllers/controller.item.room.go
@@ -0,0 +1,32 @@
+package controllers
+
+import (
+	"net/http"
+	queries_game "shared-library/database/queries/queries-game"
+	"shared-library/interceptors"
+	utils "shared-library/utils"
+)
+
+func GetAllRoomsController(w http.ResponseWriter, r *http.Request) {
+	// -> Start http conn
+	// This http connection 'll may be change with ws conn
+	w.Header().Set("Content-Type", "text/plain")
+
+	// Check method
+	checkMethod := utils.HttpMethodSet(http.MethodGet, r)
+	if checkMethod != true {
+		utils.HandleError(w, http.StatusMethodNotAllowed, "Method error expected method "+http.MethodPost)
+		return
+	}
+
+	// Check access key
+	presenceOfKey := interceptors.AccessKeyCheck(w, r)
+	if presenceOfKey == false {
+		utils.HandleError(w, http.StatusBadRequest, "Error: Required access key, check your 'access key'")
+		return
+	}
+
+	room := queries_game.GetAllRoom()
+	utils.HandleSuccess(w, room)
+	return
+}
