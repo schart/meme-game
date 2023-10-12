a693d910701e59a3c81102e6dc722165cb40d66d
Heja <arslanheja202@gmail.com>
2023-10-12 11:57:40 +0300

    re-modified for mux

--- meme/controllers/controller.item.photo.go
+++ meme/controllers/controller.item.photo.go
@@ -4,6 +4,9 @@ import (
 	"net/http"
 	db_queries "shared-library/database/queries/queries-meme"
 	utils "shared-library/utils"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 func PhotoItemsController(w http.ResponseWriter, r *http.Request) {
@@ -16,18 +19,27 @@ func PhotoItemsController(w http.ResponseWriter, r *http.Request) {
 
 	// Load enviroments
 	utils.EnvLoader()
-
 	// Check count param in url parameters
-	count := r.URL.Query().Get("count")
+	count := mux.Vars(r)["count"]
 	if count == "" {
 		utils.HandleError(w, http.StatusMethodNotAllowed, "Count is needed for get the  records of texts")
 	}
 
-	rows, err := db_queries.PhotoGetByCount(count)
+	// Parse the `count` variable as an integer.
+	countInt, err := strconv.Atoi(count)
+
+	// Check if the `count` variable could be parsed as an integer.
+	if err != nil {
+		utils.HandleError(w, http.StatusBadRequest, "Count must be an integer")
+		return
+	}
+
+	rows, err := db_queries.PhotoGetByCount(countInt)
 	if err != nil {
 		utils.HandleError(w, http.StatusMethodNotAllowed, "Error: "+err.Error())
 		return
 	}
+
 	utils.HandleSuccess(w, rows)
 	return
 }
