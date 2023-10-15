a52f081c4db154d0a2d1f3f66adc265d64306906
Heja <arslanheja202@gmail.com>
2023-10-15 20:06:31 +0300

    edited response parameters

--- shared-library/utils/util.turn.websocket.response.go
+++ shared-library/utils/util.turn.websocket.response.go
@@ -6,13 +6,13 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-func HandleErrorWS(conn *websocket.Conn, method any, message string) {
-
-	response := NewResponse(false, http.StatusFailedDependency, message, []string{})
+func HandleErrorWS(conn *websocket.Conn, message string) {
+	response := NewResponse(false, http.StatusExpectationFailed, message, []string{})
 	conn.WriteJSON(response)
+
 }
 
-func HandleSuccessWS(conn *websocket.Conn, method any) {
-	response := NewResponse(true, http.StatusOK, "Successfuly proccess", []string{})
+func HandleSuccessWS(conn *websocket.Conn, params []string) {
+	response := NewResponse(true, http.StatusOK, "Successfuly proccess", params)
 	conn.WriteJSON(response)
 }
