f75be85aeae6cc978f38e6747989abc30b522891
Heja <arslanheja202@gmail.com>
2023-11-02 02:20:51 +0300

    edited comment line

--- shared-library/websocket-connection/connection.websocket.go
+++ shared-library/websocket-connection/connection.websocket.go
@@ -14,10 +14,11 @@ var upgrader = websocket.Upgrader{
 
 /*
 
-	@ We set upgrader for use in connect 
+	@ We set upgrader for use in connect
 	@ Because we need upgrade the http connection to websocket connection
 
 */
+
 func Connect(w http.ResponseWriter, r *http.Request) *websocket.Conn {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
